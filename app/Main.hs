79b1693234c90affa872c9dc9e0d6894caf0a503
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-06-11 14:08:33 -0400

    build

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,46 @@
 module Main where
 
+import Options.Applicative
+import System.Process.Typed
+
+
 main :: IO ()
 main =
-  pure ()
+  join (customExecParser parserPrefs (info parser (progDesc description)))
+
+  where
+    description :: [Char]
+    description =
+      "hs - a haskell multitool"
+
+    parserPrefs :: ParserPrefs
+    parserPrefs =
+      ParserPrefs
+        { prefMultiSuffix = ""
+        , prefDisambiguate = False
+        , prefShowHelpOnError = True
+        , prefShowHelpOnEmpty = True
+        , prefBacktrack = True
+        , prefColumns = 80
+        }
+
+parser :: Parser (IO ())
+parser =
+  hsubparser
+    (fold
+      [ command "build" (info buildParser (progDesc "Build"))
+      ])
+
+buildParser :: Parser (IO ())
+buildParser =
+  doBuild
+    <$> pure False
+
+  where
+    doBuild :: Bool -> IO ()
+    doBuild optimize =
+      runProcess_ (shell cmd)
+      where
+        cmd :: [Char]
+        cmd =
+          "cabal v2-build -O" ++ (if optimize then "1" else "0")
---
+++ src/Prelude.hs
@@ -0,0 +1,12 @@
+module Prelude
+  ( module X
+  ) where
+
+import BasePrelude            as X
+import Control.Applicative    as X
+import Control.Monad          as X
+import Control.Monad.IO.Class as X
+import Data.Foldable          as X
+import Data.Function          as X
+import Data.Kind              as X (Type)
+import GHC.Generics           as X (Generic)
